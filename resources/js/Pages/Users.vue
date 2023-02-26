67eb6b01634d9d573704abd1771b7aadc11fc3df
kickinbrain <kickinbrain@gmail.com>
2023-02-26 17:54:56 +0400

    completed episode 16 - An Important SPA Security Concern

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,5 +1,16 @@
 <template>
         <Head title="Users" />
         <h2 class="text-xl font-bold">Users</h2>
+    <ul>
+        <li
+            v-for="user in users"
+            :key="user.id"
+            v-text="user.name">
+        </li>
+    </ul>
+
 </template>
 
+<script setup>
+defineProps({users: Array})
+</script>
