f567d19338f5019505eed8c8f9045853ce2323b4
kickinbrain <kickinbrain@gmail.com>
2023-02-28 16:33:10 +0400

    completed episode 18 - Filtering, State, and Query Strings

--- public/js/resources_js_Pages_Users_vue.js
+++ public/js/resources_js_Pages_Users_vue.js
@@ -12,13 +12,32 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony import */ var _Shared_Pagination__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../Shared/Pagination */ "./resources/js/Shared/Pagination.vue");
+/* harmony import */ var _inertiajs_inertia__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @inertiajs/inertia */ "./node_modules/@inertiajs/inertia/dist/index.js");
+
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   components: {
-    Pagination: _Shared_Pagination__WEBPACK_IMPORTED_MODULE_0__["default"]
+    Pagination: _Shared_Pagination__WEBPACK_IMPORTED_MODULE_0__["default"],
+    Inertia: _inertiajs_inertia__WEBPACK_IMPORTED_MODULE_1__.Inertia
   },
   props: {
-    users: Object
+    users: Object,
+    filters: Object
+  },
+  data: function data() {
+    return {
+      search: this.filters.search
+    };
+  },
+  watch: {
+    search: function search(value) {
+      this.$inertia.get('/users', {
+        search: value
+      }, {
+        preserveState: true,
+        replace: true
+      });
+    }
   }
 });
 
@@ -61,20 +80,30 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm-bundler.js");
 
-var _hoisted_1 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h2", {
+var _hoisted_1 = {
+  "class": "flex justify-between mb-6"
+};
+var _hoisted_2 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("h2", {
   "class": "text-xl font-bold"
 }, "Users", -1 /* HOISTED */);
-var _hoisted_2 = ["textContent"];
+var _hoisted_3 = ["textContent"];
 function render(_ctx, _cache, $props, $setup, $data, $options) {
   var _component_Head = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("Head");
   var _component_Pagination = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("Pagination");
   return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_Head, {
     title: "Users"
-  }), _hoisted_1, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("ul", null, [((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)($props.users.data, function (user) {
+  }), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_1, [_hoisted_2, (0,vue__WEBPACK_IMPORTED_MODULE_0__.withDirectives)((0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("input", {
+    type: "text",
+    "onUpdate:modelValue": _cache[0] || (_cache[0] = function ($event) {
+      return $data.search = $event;
+    }),
+    placeholder: "Search....",
+    "class": "border px-2 rounded-lg"
+  }, null, 512 /* NEED_PATCH */), [[vue__WEBPACK_IMPORTED_MODULE_0__.vModelText, $data.search]])]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("ul", null, [((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(true), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)($props.users.data, function (user) {
     return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("li", {
       key: user.id,
       textContent: (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(user.name)
-    }, null, 8 /* PROPS */, _hoisted_2);
+    }, null, 8 /* PROPS */, _hoisted_3);
   }), 128 /* KEYED_FRAGMENT */))]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" PAGINATOR "), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)(_component_Pagination, {
     links: $props.users.links
   }, null, 8 /* PROPS */, ["links"])], 64 /* STABLE_FRAGMENT */);
--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,6 +1,11 @@
 <template>
         <Head title="Users" />
-        <h2 class="text-xl font-bold">Users</h2>
+
+        <div class="flex justify-between mb-6">
+            <h2 class="text-xl font-bold">Users</h2>
+
+            <input type="text" v-model="search" placeholder="Search...." class="border px-2 rounded-lg">
+        </div>
     <ul>
         <li
             v-for="user in users.data"
@@ -16,11 +21,27 @@
 
 <script>
 import Pagination from "../Shared/Pagination";
+import {Inertia} from "@inertiajs/inertia";
 export default {
-    components: {Pagination},
+    components: {Pagination, Inertia},
     props:{
-        users: Object
-    }
+        users: Object,
+        filters: Object
+    },
+   data(){
+        return {
+            search: this.filters.search
+        }
+   },
+   watch: {
+        search(value){
+            this.$inertia.get('/users', {search: value}, {
+                preserveState: true,
+                replace: true
+            })
+        }
+   }
+
 }
 
 </script>
